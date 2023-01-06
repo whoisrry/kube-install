d9fae45a8ecb86821bd906fde4bc73444fe6b43d
whoisrry <77581185+whoisrry@users.noreply.github.com>
2023-01-06 18:43:01 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+apt install wget vim git curl apt-transport-https gnupg2 software-properties-common ca-certificates -y
+curl -fsSL  https://packages.cloud.google.com/apt/doc/apt-key.gpg| gpg --dearmor -o /etc/apt/trusted.gpg.d/k8s.gpg
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+add-apt-repository "deb https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#swapoff -a
+#sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+tee /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sysctl --system
+apt update
+apt install -y containerd.io
+containerd config default |  tee /etc/containerd/config.toml >/dev/null 2>&1
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd
+apt install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
